896e971c23c07ef3ca149c05a3629451f849815c
Bartosz Inglot <jhi@o2.pl>
2011-09-10 17:57:28 +0200

    Provisional version of a feature that runs a particular Access DB and hides unwanted features.

---
+++ WindowsFormsApplication1/Features/MsAccessView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Office.Interop.Access;
+using MsAccessRestrictor.Interfaces;
+using System.Runtime.InteropServices;
+
+namespace MsAccessRestrictor.Features {
+    public class MsAccessView : IFeature, IDisposable {
+        Application _accessApp;
+
+        public void Run() {
+            //_accessApp = (Application)Marshal.GetActiveObject("Access.Application");
+            _accessApp = new Application();
+            _accessApp.OpenCurrentDatabase(@"C:\Users\Bart\Documents\Database1.accdb");
+            _accessApp.DoCmd.ShowToolbar("Ribbon", AcShowToolbar.acToolbarNo);
+            _accessApp.RunCommand(AcCommand.acCmdWindowHide);
+            _accessApp.Visible = true;
+        }
+
+        public void Clear() {
+            if (_accessApp == null) {
+                return;
+            }
+
+            _accessApp.DoCmd.ShowToolbar("Ribbon");
+            _accessApp.RunCommand(AcCommand.acCmdWindowUnhide);
+        }
+
+        public void Dispose() {
+            if (_accessApp == null) {
+                return;
+            }
+
+            Marshal.ReleaseComObject(_accessApp);
+        }
+    }
+}
