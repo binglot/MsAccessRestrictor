af9f98271962c959c778fde261937e67f0f6c455
Bartosz Inglot <jhi@o2.pl>
2011-09-22 19:02:18 +0100

    The class to do checks when the application starts.

---
+++ WindowsFormsApplication1/StartupChecker.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using MsAccessRestrictor.Interfaces;
+using MsAccessRestrictor.Properties;
+using MsAccessRestrictor.Wrappers;
+
+namespace MsAccessRestrictor {
+    public class StartupChecker {
+        private readonly Settings _settings;
+        private readonly IMessageWriter _messageWriter;
+        private readonly IProcess _process;
+
+        public StartupChecker()
+            : this(Settings.Default, new MessageWriter(), new ProcessBase()) {
+            //
+        }
+
+        internal StartupChecker(Settings settings, IMessageWriter messageWriter, IProcess process) {
+            _settings = settings;
+            _messageWriter = messageWriter;
+            _process = process;
+        }
+
+        public void Run() {
+            CheckOpenMsAccess();
+
+        }
+
+        private void CheckOpenMsAccess() {
+            const string closeError = "Close MsAccess before running the application or change your settings.";
+            const string openError = "Open MsAccess before running the application or change your settings.";
+            const string moreThanOneError =
+                "Leave only one MsAccess open before running the application or change your settings.";
+
+            var processes = _process.GetProcessesByName("MSACCESS");
+
+            if (_settings.OpenMsAccess)
+            {
+                // Show error if there's a running MsAccess
+                if (processes.Length != 0)
+                {
+                    _messageWriter.ShowError(closeError);
+                }
+            }
+            else
+            {
+                if (processes.Length == 0)
+                {
+                    _messageWriter.ShowError(openError);
+                }
+                else if (processes.Length > 1)
+                {
+                    _messageWriter.ShowError(moreThanOneError);
+                }
+            }
+        }
+    }
+}
