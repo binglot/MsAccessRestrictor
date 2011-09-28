15efa78192d77e65997251ecfd7d90d768979033
Bartosz Inglot <jhi@o2.pl>
2011-09-28 08:41:44 +0100

    StartUpChecker is basically finished but needs writing unit tests for the other 2 methods.

--- WindowsFormsApplication1/Main/StartUpChecker.cs
+++ WindowsFormsApplication1/Main/StartUpChecker.cs
@@ -1,4 +1,6 @@
-﻿using MsAccessRestrictor.Interfaces;
+﻿using System.IO;
+using System.Windows.Forms;
+using MsAccessRestrictor.Interfaces;
 using MsAccessRestrictor.Properties;
 using MsAccessRestrictor.Wrappers;
 
@@ -7,6 +9,7 @@ namespace MsAccessRestrictor.Main {
         private readonly Settings _settings;
         private readonly IMessageWriter _messageWriter;
         private readonly IProcess _process;
+
 
         public StartUpChecker()
             : this(Settings.Default, new MessageWriter(), new ProcessBase()) {
@@ -20,37 +23,61 @@ namespace MsAccessRestrictor.Main {
         }
 
         public void Run() {
+            CheckNumberOfInstances();
             CheckOpenMsAccess();
+            CheckOpenDbFile();
+        }
 
+        private void CheckNumberOfInstances() {
+            const string alreadyRunningError = "There's already one instance of the application running!";
+            
+            var processes = _process.GetProcessesByName("MsAccessRestrictor");
+
+            if (processes.Length != 1)
+            {
+                ShowErrorAndStopRunning(alreadyRunningError);
+            }
+        }
+
+        private void CheckOpenDbFile() {
+            const string openFileError = "Opening the database file failed. Check your settings.";
+
+            if (!_settings.OpenMsAccess || !_settings.OpenDbFile) {
+                return;
+            }
+
+            if (!File.Exists(_settings.DbFilePath)) {
+                ShowErrorAndStopRunning(openFileError);
+            }
         }
 
         private void CheckOpenMsAccess() {
             // TODO: put these constants in the Resources
-            const string closeError = "Close MsAccess before running the application or change your settings.";
-            const string openError = "Open MsAccess before running the application or change your settings.";
-            const string moreThanOneError = "Leave only one MsAccess window open before running the application or change your settings.";
+            const string closeError = "Close Microsoft Access before running the application or change your settings.";
+            const string openError = "Open Microsoft Access before running the application or change your settings.";
+            const string moreThanOneError = "Leave only one Microsoft Access window open before running the application or change your settings.";
 
             var processes = _process.GetProcessesByName("MSACCESS");
 
-            if (_settings.OpenMsAccess)
-            {
+            if (_settings.OpenMsAccess) {
                 // Show error if there's a running MsAccess
-                if (processes.Length != 0)
-                {
-                    _messageWriter.ShowError(closeError);
+                if (processes.Length != 0) {
+                    ShowErrorAndStopRunning(closeError);
                 }
             }
-            else
-            {
-                if (processes.Length == 0)
-                {
-                    _messageWriter.ShowError(openError);
+            else {
+                if (processes.Length == 0) {
+                    ShowErrorAndStopRunning(openError);
                 }
-                else if (processes.Length > 1)
-                {
-                    _messageWriter.ShowError(moreThanOneError);
+                else if (processes.Length > 1) {
+                    ShowErrorAndStopRunning(moreThanOneError);
                 }
             }
+        }
+
+        private void ShowErrorAndStopRunning(string message) {
+            _messageWriter.ShowError(message);
+            Application.Exit();
         }
     }
 }
